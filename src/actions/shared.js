4f166fd96357871f4c14f8911702f0d65e92f875
Cristoval Fonseca <cfonseca@protonmail.com>
2018-09-12 15:40:42 -0700

    for this project, hard-coded an authedUser const

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,8 @@ import {getInitialData} from "../utils/api"
 import {receiveTweets} from "./tweets";
 import {receiveUsers} from "./users";
 
+const AUTHED_ID = 'tylermcginnis'
+
 export function handleInitialData() {
     return (dispatch) => {
         return getInitialData()
